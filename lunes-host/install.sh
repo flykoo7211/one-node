cd345e577c034df54cb2b39a46736dde96c38d41
flykoo7211 <86999643+flykoo7211@users.noreply.github.com>
2025-10-01 03:53:03 +0800

    Update install.sh

--- lunes-host/install.sh
+++ lunes-host/install.sh
@@ -5,16 +5,16 @@ PORT="${PORT:-10008}"
 UUID="${UUID:-2584b733-9095-4bec-a7d5-62b473540f7a}"
 HY2_PASSWORD="${HY2_PASSWORD:-vevc.HY2.Password}"
 
-curl -sSL -o app.js https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/app.js
-curl -sSL -o package.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/package.json
+curl -sSL -o app.js https://raw.githubusercontent.com/flykoo7211/one-node/refs/heads/main/lunes-host/app.js
+curl -sSL -o package.json https://raw.githubusercontent.com/flykoo7211/one-node/refs/heads/main/lunes-host/package.json
 
 mkdir -p /home/container/xy
 cd /home/container/xy
-curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v25.9.11/Xray-linux-64.zip
+curl -sSL -o Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v25.8.3/Xray-linux-64.zip
 unzip Xray-linux-64.zip
 rm Xray-linux-64.zip
 mv xray xy
-curl -sSL -o config.json https://raw.githubusercontent.com/vevc/one-node/refs/heads/main/lunes-host/xray-config.json
+curl -sSL -o config.json https://raw.githubusercontent.com/flykoo7211/one-node/refs/heads/main/lunes-host/xray-config.json
 sed -i "s/10008/$PORT/g" config.json
 sed -i "s/YOUR_UUID/$UUID/g" config.json
 keyPair=$(./xy x25519)
@@ -23,7 +23,7 @@ publicKey=$(echo "$keyPair" | grep "Public key" | awk '{print $3}')
 sed -i "s/YOUR_PRIVATE_KEY/$privateKey/g" config.json
 shortId=$(openssl rand -hex 4)
 sed -i "s/YOUR_SHORT_ID/$shortId/g" config.json
-vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.cloudflare.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
+vlessUrl="vless://$UUID@$DOMAIN:$PORT?encryption=none&flow=xtls-rprx-vision&security=reality&sni=www.hp.com&fp=chrome&pbk=$publicKey&sid=$shortId&spx=%2F&type=tcp&headerType=none#lunes-reality"
 echo $vlessUrl > /home/container/node.txt
 
 mkdir -p /home/container/h2
